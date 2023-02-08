a09b65d019d54942943e1b8b4d5685c988e44deb
Rubén García-Redondo Marín <rubengrm@icloud.com>
2023-02-08 14:15:41 +0100

    Preparación Microservicios

---
+++ Microservicio-Pedidos/src/main/kotlin/biques/dam/es/Application.kt
@@ -0,0 +1,13 @@
+package biques.dam.es
+
+import io.ktor.server.application.*
+import biques.dam.es.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSecurity()
+    configureRouting()
+}
