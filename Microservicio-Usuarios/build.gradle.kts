d6e6d2e0b7f187f70396f236913be10c872a868e
Alejandro Sánchez Monzón <alejandrosanchezmonzon1@gmai.com>
2023-02-13 14:10:11 +0100

    Modelo, DTO, Excepciones , mappers y repositorios para Usuario.

--- Microservicio-Usuarios/build.gradle.kts
+++ Microservicio-Usuarios/build.gradle.kts
@@ -1,76 +1,78 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-val logbackclassic_version: String by project
-val bcrypt_version: String by project
-val koin_ksp_version: String by project
-val koin_ktor_version: String by project
-val micrologging_version: String by project
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.10"
-    id("io.ktor.plugin") version "2.2.3"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
-    id("com.google.devtools.ksp") version "1.8.0-1.0.8"
+	id("org.springframework.boot") version "3.0.2"
+	id("io.spring.dependency-management") version "1.1.0"
+	kotlin("jvm") version "1.7.22"
+	kotlin("plugin.spring") version "1.7.22"
 }
 
-group = "dam.biques.es"
-version = "0.0.1"
-application {
-    mainClass.set("io.ktor.server.netty.EngineMain")
-
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
-}
+group = "es.dam.biques"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    // Ktor Core
-    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+	implementation("org.springframework.boot:spring-boot-starter")
 
-    // Auth JWT
-    implementation("io.ktor:ktor-server-auth-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-host-common-jvm:$ktor_version")
+	// H2 Database
+	runtimeOnly("com.h2database:h2")
 
-    // JSON content negotiation
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+	// Dependencias de Spring Boot y Spring Data Reactive
+	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 
-    // Content validation
-    implementation("io.ktor:ktor-server-request-validation:$ktor_version")
+	// Spring Caché
+	implementation("org.springframework.boot:spring-boot-starter-cache")
 
-    // Caching Headers
-    implementation("io.ktor:ktor-server-caching-headers-jvm:$ktor_version")
+	// Spring Validador
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 
-    // Compression
-    implementation("io.ktor:ktor-server-compression-jvm:$ktor_version")
-    // CORS
-    implementation("io.ktor:ktor-server-cors-jvm:$ktor_version")
+	// Spring WebFlux
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
 
-    // WebSockets
-    implementation("io.ktor:ktor-server-websockets-jvm:$ktor_version")
+	//Spring WebSocket
+	implementation("org.springframework.boot:spring-boot-starter-websocket")
 
-    // Swagger
-    implementation("io.ktor:ktor-server-swagger:$ktor_version")
+	// Kotlin
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
-    // Logging
-    implementation("ch.qos.logback:logback-classic:$logbackclassic_version")
-    implementation("io.github.microutils:kotlin-logging-jvm:$micrologging_version")
+	// Swagger
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
 
-    // Koin
-    implementation("io.insert-koin:koin-ktor:$koin_ktor_version")
-    implementation("io.insert-koin:koin-logger-slf4j:$koin_ktor_version")
-    implementation("io.insert-koin:koin-annotations:$koin_ksp_version")
-    ksp("io.insert-koin:koin-ksp-compiler:$koin_ksp_version")
+	// Para mis logs
+	implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
 
-    implementation("org.mindrot:jbcrypt:$bcrypt_version")
+	// JWT
+	implementation("com.auth0:java-jwt:4.2.1")
 
-    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+	// Bcrypt
+	implementation("org.mindrot:jbcrypt:0.4")
+
+	// Test
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+
+	// Corrutinas
+	testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+
+	// Mockk
+	testImplementation("com.ninja-squad:springmockk:4.0.0")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
 }
---
+++ Microservicio-Usuarios/src/main/kotlin/es/dam/biques/microserviciousuarios/MicroservicioUsuariosApplication.kt
@@ -0,0 +1,11 @@
+package es.dam.biques.microserviciousuarios
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MicroservicioUsuariosApplication
+
+fun main(args: Array<String>) {
+	runApplication<MicroservicioUsuariosApplication>(*args)
+}
